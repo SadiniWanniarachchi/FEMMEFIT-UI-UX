e822cac371bc01f2ebacd59c6b383b00d1420d3a
SadiniWanniarachchi <sadiiw17@gmail.com>
2024-12-14 02:04:44 +0530

    Final Updated web view

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-
   ],
   theme: {
     extend: {
@@ -14,24 +12,38 @@ export default {
       },
 
       fontFamily: {
-         poppins: [ "Poppins" , "sans-serif"],
-         averia: ["Averia Serif Libre" , "serif"],
-
+        poppins: ["Poppins", "sans-serif"],
+        averia: ["Averia Serif Libre", "serif"],
       },
 
       container: {
-            center: true,
-            padding:{
-              DEFAULT: "1rem",
-              sm: "2rem",
-              lg: "4rem",
-              xl: "5rem",
-              "2xl": "6rem",
-            }
-      }
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
 
+      // Extend the keyframes and animation
+      keyframes: {
+        waveAnimation: {
+          '0%': {
+            transform: 'translateX(0)',
+          },
+          '100%': {
+            transform: 'translateX(-100%)',
+          },
+        },
+      },
+
+      // Add the wave animation
+      animation: {
+        waveAnimation: 'waveAnimation 10s infinite linear',
+      },
     },
   },
   plugins: [],
 }
-
